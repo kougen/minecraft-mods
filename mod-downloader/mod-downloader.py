99797c1d04f910fe63a73bfc1650b8bb508336cc
Joshua Hegedus <jhegedus9@gmail.com>
2023-01-22 23:58:20 +0100

    pack update

--- mod-downloader/mod-downloader.py
+++ mod-downloader/mod-downloader.py
@@ -2,7 +2,7 @@ from mod import ModManager
 from baselib import minecraft_path
 from modoperations import copy_to_target
 
-manager = ModManager()
+manager = ModManager(True)
 pack = manager.select_mod_packs()
 if pack is not None:
 	copy_to_target(manager, pack.pack_content, minecraft_path())
